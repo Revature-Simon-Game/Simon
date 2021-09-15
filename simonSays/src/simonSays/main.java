3d369d951035b5a74a5bb7a60447069f69800a5b
quintonliang <46580715+quintonliang@users.noreply.github.com>
2021-09-15 10:41:36 -0500

    basic frame

---
+++ simonSays/src/simonSays/main.java
@@ -0,0 +1,45 @@
+package simonSays;
+
+import java.util.*;
+
+public class main {
+
+	public static void main(String[] args) {
+		Scanner user = new Scanner(System.in);
+		List<Integer> pattern = new ArrayList();
+		boolean loss = false;
+		Random rand = new Random();
+		int input = 0;
+		
+		System.out.println("Simon says with a number between 1-4");
+		
+		while (!loss) {
+			pattern.add(rand.nextInt((4-1) + 1) + 1);
+			System.out.println(pattern);
+			for (int i : pattern) {
+				if (loss) {
+					System.out.println("1st loss");
+					break;
+				}
+				
+				if (user.hasNextInt()) {
+					input = user.nextInt();
+				}
+				else {
+					loss = true;
+				}
+				
+				if (input != i) {
+					loss = true;
+					System.out.println("2nd loss" + input + " " + i);
+					break;
+				}
+			}
+			if(loss) {
+				System.out.println("You lose! Your record is " + pattern.size());
+			}
+		}
+		
+	}
+
+}
